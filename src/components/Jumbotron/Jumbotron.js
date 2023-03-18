0afcf98355cd7dc611d833b2b93fc755456a719c
Rob S <me@rob-saunders.co.uk>
2023-03-18 20:06:11 +0000

    Restyled the jumbotron with a margin as it was not 'floating'

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -1,17 +1,16 @@
 import React from 'react'
-// import { homepageContent } from '../../pages/HomePage/HomePageContent'
 
 function Jumbotron(data) {
 
   return (
-      <>
-        <div className="jumbotron rockBackground">
-          <div className="jumboCopy">
-              <h1>{data.h1}</h1>
-              <p>{data.p}</p>
-          </div>
+    <>
+      <div className="jumbotron rockBackground">
+        <div className="jumboCopy">
+            <h1>{data.h1}</h1>
+            <p>{data.p}</p>
         </div>
-      </>
+      </div>
+    </>
   )
 }
 
