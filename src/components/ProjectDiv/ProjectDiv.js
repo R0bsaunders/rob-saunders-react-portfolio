235c37c57794e751b28158f4590d40cdb85230b1
Rob S <me@rob-saunders.co.uk>
2023-03-23 09:04:28 +0000

    The project cards were displaying as one column on lg screens but working fine on all other breakpoints. Discovered an erroneous cols-lg-12 in a container in the Project Div component

--- src/components/ProjectDiv/ProjectDiv.js
+++ src/components/ProjectDiv/ProjectDiv.js
@@ -5,17 +5,18 @@ const liClass = "d-flex align-items-center me-3"
 
 function ProjectDiv(props) {
   return (
-    <a href={props.appLink} target="_blank">
-        <div className="col-lg-12 portfolio-height p-2">
+
+        <div className="portfolio-height p-2">
             <div className="work-card card card-cover h-100 overflow-hidden text-bg-dark shadow-lg" id={props.divId} >
 
+              <a href={props.appLink} target="_blank">
                 <h3 className="mb-4 h3-portfolio">{props.appName}</h3>
 
                 <div id="card-description">
 
                     <p>{props.appDescription}</p>
                 </div>
-
+                </a>
                 <div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
 
                     <ul className="work-ul d-flex list-unstyled mt-auto">
@@ -39,7 +40,6 @@ function ProjectDiv(props) {
                 </div>
             </div>
         </div>
-    </a>
   )};
 
 export default ProjectDiv;
