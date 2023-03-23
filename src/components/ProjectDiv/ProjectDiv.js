8bd56a513c657545a6848c75dea9467d916df26f
Rob S <me@rob-saunders.co.uk>
2023-03-23 15:21:12 +0000

    Tidied project DIV

--- src/components/ProjectDiv/ProjectDiv.js
+++ src/components/ProjectDiv/ProjectDiv.js
@@ -1,28 +1,37 @@
-import React from 'react'
-import '../../styles/style.css'
+import React from 'react';
+import '../../styles/style.css';
 
-const liClass = "d-flex align-items-center me-3"
+const liClass = "d-flex align-items-center me-3";
 
 function ProjectDiv(props) {
+
+    // Function to display a GitHub badge and link to repo only if one is present in the project.js file
     function checkGitHub(data) {
+
         if(data ==="GitHub") {
-            console.log("TRUE");
-            return <a href={props.github} className="gitLink" target="_blank">{props.tech3} <i className="bi bi-box-arrow-up-right"></i></a>
-        }
-        return data
-    }
+
+            return <a href={props.github} className="gitLink" target="_blank">{props.tech3} <i className="bi bi-box-arrow-up-right"></i></a>;
+
+        };
+
+        return data;
+    };
     
   return (
 
         <div className="p-2">
             <div className="work-card card card-cover h-100 overflow-hidden text-bg-dark shadow-lg" id={props.divId} >
 
-                              <h3 className="mb-4 h3-portfolio">{props.appName}</h3>
+                <h3 className="mb-4 h3-portfolio">{props.appName}</h3>
 
                 <div id="card-description">
 
                     <p>{props.appDescription}</p>
-                 <a href={props.appLink} target="_blank"><button className="btn btn-project">View App</button></a>
+
+                    <a href={props.appLink} target="_blank">
+                        <button className="btn btn-project">View App</button>
+                    </a>
+
                 </div>
 
                 <div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
@@ -31,12 +40,12 @@ function ProjectDiv(props) {
 
                         <li className={liClass}>
                             <i className={props.icon1}></i>
-                            <small>{props.tech1}</small>
+                            {props.tech1}
                         </li>
 
                         <li className={liClass}>
                             <i className={props.icon2}></i>
-                            <small>{props.tech2}</small>
+                            {props.tech2}
                         </li>
 
                         <li className={liClass}>
