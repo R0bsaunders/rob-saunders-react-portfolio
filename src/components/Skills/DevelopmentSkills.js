5c900a27f3620e13c230ca9f0117c85c265ca68c
Rob S <me@rob-saunders.co.uk>
2023-03-23 15:25:29 +0000

    skills components tidied

--- src/components/Skills/DevelopmentSkills.js
+++ src/components/Skills/DevelopmentSkills.js
@@ -6,22 +6,25 @@ function DevelopmentSkills() {
 
   return (
     <article id="skills">
-        <div className="container px-4 py-5" id="icon-grid">
-            <h2 className="pb-2 border-bottom" id="skills">Website Development Skills</h2>
-            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4 py-5">
 
-                {homepageContent[2].map( s => <Skill 
-                icon={s.icon}
-                h3={s.h3}
-                p={s.p}
-                key={s.id} />)}
-                
-            </div>
+      <div className="container px-4 py-5" id="icon-grid">
+
+        <h2 className="pb-2 border-bottom" id="skills">Website Development Skills</h2>
+        
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4 py-5">
+
+          {homepageContent[2].map( s => <Skill 
+          icon={s.icon}
+          h3={s.h3}
+          p={s.p}
+          key={s.id} />)}
+            
         </div>
+      </div>
     </article>
+  );
+};
 
-  )
-}
-export default DevelopmentSkills
+export default DevelopmentSkills;
 
 
--- src/components/Skills/Skill.js
+++ src/components/Skills/Skill.js
@@ -1,19 +1,20 @@
-import React from 'react'
+import React from 'react';
 
 function Skill(props) {
   return (
 
     <div className="col d-flex align-items-start">
-        <i className={props.icon}></i>
+      
+      <i className={props.icon}></i>
 
-        <div>
-            <h3 className="fw-bold mb-0 fs-4">{props.h3}</h3>
-            <p>{props.p}</p>
-        </div>
+      <div>
 
+        <h3 className="fw-bold mb-0 fs-4">{props.h3}</h3>
+        <p>{props.p}</p>
+
+      </div>
     </div>
+  );
+};
 
-  )
-}
-
-export default Skill
+export default Skill;
