d19bb961a25b271b221f226a3114ddd4c5e0d220
Rob S <me@rob-saunders.co.uk>
2023-03-23 15:15:39 +0000

    Tidied the jumbo tron code

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -1,15 +1,20 @@
+import React from "react";
+
 function Jumbotron(data) {
 
   return (
     <>
       <div className="jumbotron rockBackground">
+
         <div className="jumboCopy">
+
             <h1>{data.h1}</h1>
             <p>{data.p}</p>
+            
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Jumbotron
+export default Jumbotron;
