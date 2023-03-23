564cad6e61f44ed5c564de605d606788fbbc6fd4
Rob S <me@rob-saunders.co.uk>
2023-03-23 15:34:45 +0000

    tidied homepage code

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -9,16 +9,20 @@ function HomePage() {
   return (
     <>
       <section id="hero">
+
         <Jumbotron
           h1={homepageContent[0].jumbotronH1}
           p={homepageContent[0].jumbotronP} />
+
       </section>
+
       <LeftImageBlock 
         h2={homepageContent[3].aboutH2}
         image={homepageContent[3].image}
         alt={homepageContent[3].imageAlt}
         p={homepageContent[3].aboutP}
       />
+      
       <DevelopmentSkills />
 
     </>
