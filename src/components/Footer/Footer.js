80b4264e6d24ce1eaa9713a82436b96f6621c981
Rob S <me@rob-saunders.co.uk>
2023-03-24 00:44:44 +0000

    Added JavaScript to update the copyright year in the footer to the current year automatically

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { HashLink as Link } from 'react-router-hash-link';
 
 function Footer() {
+
+  //Code to update the copyright year automatically
+  let date =  new Date().getFullYear();
+
   return (
     <footer>
 
       <div>
         <Link to="/#"><img alt="Rob Saunders Website Logo" src="./assets/images/rob-saunders-logo.png" /></Link>
-        <p>2022 Copyright Rob Saunders</p>
+        <p>{date} Copyright Rob Saunders</p>
       </div>
 
       <div>
